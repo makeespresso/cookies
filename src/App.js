38133847d7f87969e66dea43e6c26a0a77659617
Ana S. Benedict <anabenedict12@gmail.com>
2022-02-15 15:23:35 -0700

    update

--- src/App.js
+++ src/App.js
@@ -95,8 +95,7 @@ function App() {
     <div className="content">
             <div className="aboutContent">
               <p>
-                Hello, UX/UI Designer and Web Developer based in
-                NYC, currently exploring Mar de Cortéz 🌊🌵🌞. I speak Spanish, English and
+              Hello, I am a Design Technologist based in NYC, currently exploring Mar de Cortéz 🌊🌵🌞. I speak Spanish, English and
                 Italian. Barista by passion and fan of Mediterranean cuisine. ✌🏼
               </p>
             </div>

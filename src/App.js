756b6e228169b3c74efa3dbcb479c274a7f0d572
makeespresso <anabenedict12@gmail.com>
2024-04-18 15:33:16 -0400

    upd

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import Navbar from './components/Navbar';
 
 // import Footer from "./components/Footer";
 import A_img from './assets/p_13.png';
+import img_localsumer from './assets/img_localsumer.png';
+import img_flekora from './assets/img_flekora.png';
+import img_etf from './assets/img_etf.png';
+import img_xoespresso from './assets/img_xoespresso.jpg';
 
 function App() {
   return (
@@ -26,21 +30,25 @@ function App() {
                 <span>Selected work</span>
               </h2>
               <ul>
+                <img className="img_style" src={img_localsumer} alt="Localsumer"/> 
                 <li className="title">Localsumer - Product Design</li>
                 <p>From <a href="https://www.behance.net/gallery/99482723/Localsumer-Business-Directory" target="_blank">ideation</a> to launch of the web app.</p>
                 Learn more about Localsumer in this <a href="https://www.youtube.com/watch?v=Tjxtfp9kzco" target="_blank">video</a>. 👈🏼
-                <p> (psst... psst... before launching we made it to round two of <a href="https://masschallenge.org/programs-mexico" target="_blank">MassChallenge Mexico</a>, and we celebrate that! 🎉)</p>
+                <p className="br_space"> (psst... psst... before launching we made it to round two of <a href="https://masschallenge.org/programs-mexico" target="_blank">MassChallenge Mexico</a>, and we celebrate that! 🎉)</p>
                 {/* <p>Also, don't forget to follow us on <a href="https://www.instagram.com/localsumer/" target="_blank">IG</a>!😉</p> */}
                 <br></br>
+                <img className="img_style" src={img_flekora} alt="Flekora"/> 
                 <li className="title">Flekora - UX/UI Design, Web Development</li>
                 <p>Ideated, <a href="https://www.behance.net/gallery/100190355/Flekora-UXUI-Case-Study" target="_blank">created</a> and programmed <a href="https://weareflekora.com/#Services" target="_blank">Flekora's website</a>.👾</p>
-                <p>Flekora is a boutique design agency offering UX/UI design, strategy, branding services. 💫</p>
+                <p className="br_space">Flekora is a boutique design agency offering UX/UI design, strategy, branding services. 💫</p>
                 <br></br>
+                <img className="img_style" src={img_etf} alt="ETF"/> 
                 <li className="title">ETF - UX/UI Design</li>
                 <p>Web App redesign 📸</p> 
                 <p>Watch the <a href="https://drive.google.com/file/d/1dii1So3FnSlWGIKlEYGQzykfzgPyjM2o/view?usp=sharing" target="_blank">final result</a>.</p>
-                <p>See the <a href="https://drive.google.com/file/d/1WnxsNw5WEzZKmNrmvGL2C5YJhvhwf1GD/view?usp=sharing" target="_blank">design process</a> 🔁 and <a href="https://drive.google.com/file/d/1GCHbtdNG2Vdc5MCP9tqimNDzB2VZwgFE/view?usp=sharing" target="_blank">visual design</a>.🎨</p>
- <br></br>
+                <p className="br_space" >See the <a href="https://drive.google.com/file/d/1WnxsNw5WEzZKmNrmvGL2C5YJhvhwf1GD/view?usp=sharing" target="_blank">design process</a> 🔁 and <a href="https://drive.google.com/file/d/1GCHbtdNG2Vdc5MCP9tqimNDzB2VZwgFE/view?usp=sharing" target="_blank">visual design</a>.🎨</p>
+                <br></br>
+                <img className="img_style" src={img_xoespresso} alt="XO Espresso"/> 
                 <li className="title">XO Espresso Bar - UX/UI Consultation </li>
                 <p>
                   Collaboration with specialty coffee shop with Aussie vibes and instagrammable drinks in the heart of Miami beach. 🏖
@@ -48,7 +56,7 @@ function App() {
                   <p>Take a look at the case study <a href="https://www.canva.com/design/DADua4nuPws/tWNtNjv26HwHCymlfxe0hw/view?utm_content=DADua4nuPws&utm_campaign=designshare&utm_medium=link&utm_source=viewer" target="_blank">here</a>. Also, their <a href="https://www.instagram.com/xoespressobar/" target="_blank">IG</a> is so chic! 💁🏻‍♀️☕️</p>
                   <p><a href="https://medium.com/@anamendivil/xo-espresso-bar-image-documentation-efa3eb0d0e41" target="_blank"> Image documentation</a></p>
               </ul>
-              <br></br>
+              <br className="br_space"></br>
               <br></br>
               <h2>
                 <span>Design Lead</span>
